500b1238ab3220649905316ff28f48823f4676fb
alexvc19111 <118561767+alexvc19111@users.noreply.github.com>
2026-06-03 16:41:19 -0500

    Move importRoutes to the correct position in middleware

--- server/server.js
+++ server/server.js
@@ -11,17 +11,16 @@ const sessionsRoutes      = require('./routes/sessions')
 const importRoutes        = require('./routes/import')
 
 const app = express()
-
 app.use(cors())
 
-
-app.use('/api/quizzes', importRoutes)
 app.use(express.json({ limit: '1mb' }))
 
 app.use('/api/auth',     authRoutes)
 app.use('/api/quizzes',  quizzesRoutes)
 app.use('/api/sessions', sessionsRoutes)
 app.use('/api',          questionsRoutes)
+
+app.use('/api/quizzes',  importRoutes)
 
 app.use((err, req, res, next) => {
   console.error(err)
